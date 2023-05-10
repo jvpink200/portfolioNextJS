7e94d0770fd96b5e5e9b7d37de2d30b7175c8c35
Janet Villarreal <jvillarr20@gmail.com>
2023-05-09 20:13:50 -0700

    updated with api test

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import styles from "../styles/NavBar.module.scss";
+import Image from "next/image";
+import { UilMessage, UilUserCircle } from "@iconscout/react-unicons";
+import Link from "next/link";
+export default function NavBar() {
+  return (
+    <>
+      <nav className={styles.nav}>
+        <div className="navHome">
+          <Link href="/">
+            <Image src="/logo.png" alt="logo nav" width={48} height={48} />
+          </Link>
+          <Link href="/">Home</Link>
+        </div>
+
+        <ul className={styles.containerNav}>
+          <li className={styles.navLinks}>
+            <UilUserCircle />
+            <Link href="/about">About</Link>
+          </li>
+          <li className={styles.navLinks}>
+            <UilMessage />
+            <Link href="/contact">Contact </Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import NavBar from "@/components/NavBar";
+export default function About() {
+  return (
+    <>
+      <p>
+        As a tech professional with over 4+ years ranging from having experience
+        as a support application analyst to being a
+        <span className="developer"> software developer</span>, I have
+        cultivated an impressive skill set from strong debugging skills to even
+        photoshop and figma skills.
+      </p>
+      <p>
+        I have learned to utilize cutting-edge development and techniques
+        through the use of React, Next JS, HTML, CSS/SASS,
+        Javascript/Typescript. These technical skills allow me to create
+        beautiful, dynamic, and responsive websites while writing clean code
+        that is reusable. I thrive in collaborative environments with
+        cross-functional teams from designers to software engineers to managers.
+      </p>
+      <p>Hobbies:</p>
+      <p>Video Games, Guitar, Soccer, and of course coding.</p>
+    </>
+  );
+}
