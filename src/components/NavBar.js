c423c6e27a808b2b8f0d426d359682b96a33c7ae
Janet Villarreal <jvillarr20@gmail.com>
2023-05-10 17:18:21 -0700

    updated navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,40 +4,33 @@ import { UilMessage, UilUserCircle } from "@iconscout/react-unicons";
 import Link from "next/link";
 export default function NavBar({ active }) {
   return (
-    <>
-      <nav className={styles.nav}>
-        <div className={styles.navHome}>
-          <Link href="/">
-            <Image src="/logo.png" alt="logo nav" width={48} height={48} />
-          </Link>
-          <Link href="/">
-            <span className={active == "home" ? styles.active : null}>
-              Home
+    <nav className={styles.nav}>
+      <div className={styles.navHome}>
+        <Link href="/">
+          <Image src="/logo.png" alt="logo nav" width={48} height={48} />
+        </Link>
+        <Link href="/">
+          <span className={active == "home" ? styles.active : null}>Home</span>
+        </Link>
+      </div>
+      <ul className={styles.containerNav}>
+        <li className={styles.navLinks}>
+          <UilUserCircle />
+          <Link href="/about">
+            <span className={active == "aboutActive" ? styles.active : null}>
+              About
             </span>
           </Link>
-        </div>
-
-        {/*<ul className={styles.containerNav}>
-          <li className={styles.navLinks}>
-            <UilUserCircle />
-            <Link href="/about">
-              <span className={active == "aboutActive" ? styles.active : null}>
-                About
-              </span>
-            </Link>
-          </li>
-          <li className={styles.navLinks}>
-            <UilMessage />
-            <Link href="/contact">
-              <span
-                className={active == "contactActive" ? styles.active : null}
-              >
-                Contact
-              </span>
-            </Link>
-          </li>
-  </ul>*/}
-      </nav>
-    </>
+        </li>
+        <li className={styles.navLinks}>
+          <UilMessage />
+          <Link href="/contact">
+            <span className={active == "contactActive" ? styles.active : null}>
+              Contact
+            </span>
+          </Link>
+        </li>
+      </ul>
+    </nav>
   );
 }
--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,5 +1,4 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import NavBar from "@/components/NavBar";
 export default function Document() {
   return (
     <Html>
