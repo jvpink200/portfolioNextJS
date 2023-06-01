07358b525e660792f7a6e3a7eb4e7d93daf9323f
Janet Villarreal <jvillarr20@gmail.com>
2023-05-31 20:47:24 -0700

    added hobby icons

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,5 +1,9 @@
 import NavBar from "@/components/NavBar";
 import styles from "../styles/About.module.scss";
+import Image from "next/image";
+import videoGC from "../assets/videoGC.png";
+import electricGuitar from "../assets/electricGuitar.png";
+import { UilFootball, UilBracketsCurly } from "@iconscout/react-unicons";
 export default function About() {
   return (
     <>
@@ -29,7 +33,28 @@ export default function About() {
             from designers to software engineers to managers.
           </p>
           <p className={styles.title}>Hobbies:</p>
-          <p>Video Games, Guitar, Soccer, and of course coding.</p>
+          <ul className={styles.hobbies}>
+            <li>
+              Video Games
+              <span className={styles.videoGC}>
+                <Image src={videoGC} alt="video game controller" />
+              </span>
+            </li>
+            <li>
+              Guitar
+              <span className={styles.electricGuitar}>
+                <Image src={electricGuitar} alt="video game controller" />
+              </span>
+            </li>
+            <li>
+              Soccer
+              <span className={styles.soccer}><UilFootball /></span>
+            </li>
+            <li>
+              Coding
+              <span className={styles.coding}><UilBracketsCurly /></span>
+            </li>
+          </ul>
         </div>
       </section>
     </>
