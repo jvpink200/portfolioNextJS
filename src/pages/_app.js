5128433384db30e1a3e77174331ea1f1cbb22d8f
Janet Villarreal <jvillarr20@gmail.com>
2023-07-18 15:49:33 -0700

    removed unused code

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,19 +1,5 @@
 import "@/styles/globals.scss";
-//import { useEffect, createContext, useState } from "react";
-
-//export const AuthContext = createContext();
 
 export default function App({ Component, pageProps }) {
-  // const [accessToken, setAccessToken] = useState(null);
-
-  // useEffect(() => {
-  //   const fetchAccessToken = async () => {
-  //     const res = await fetch("/api/oauth-token-petfinder");
-  //     const json = await res.json();
-  //     console.log(json);
-  //     setAccessToken(json.access_token);
-  //   };
-  //   fetchAccessToken();
-  // }, []);
   return <Component {...pageProps} />;
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,32 +1,9 @@
 import NavBar from "@/components/NavBar";
 import Herobanner from "@/components/HeroBanner";
 import Projects from "@/components/Projects";
-import Footer from "@/components/Footer";
 import Head from "next/head";
-import { useEffect, useState, useContext } from "react";
-//import { AuthContext } from "./_app";
 
 export default function Home() {
-  // const [results, setResults] = useState(null);
-  // const accessToken = useContext(AuthContext);
-
-  // useEffect(() => {
-  //   if (accessToken === null) return;
-  //   const fetchPets = async () => {
-  //     const petResults = await fetch(
-  //       "https://api.petfinder.com/v2/animals?location=24558",
-  //       {
-  //         headers: {
-  //           Authorization: `Bearer ${accessToken}`,
-  //         },
-  //       }
-  //     );
-  //     const json = await petResults.json();
-  //     setResults(json.animals);
-  //   };
-  //   fetchPets();
-  // }, [accessToken]);
-  // if (results === null) return null;
   return (
     <>
       <Head>
